4f73ffc2ae0f93636dc9f8ec38cc9508e9242000
Thabo <pro.dignitron@gmail.com>
2023-10-19 17:26:07 +0200

    initial commit

--- app.py
+++ app.py
@@ -81,7 +81,6 @@ def load_user(user_id):
     #     return db.query(user_class.cls_name).get(current_user.id)
     #     #print("-------------------------No Class User: ",user_class.cls_name)
 
-app.config['SECRET_KEY'] = 'f9ec9f35fbf2a9d8b95f9bffd18ba9a1'
 
 
 
