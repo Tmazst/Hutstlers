3be8b3c18b5ab9f61f17b512ddfe5d8d0acdddb9
Thabo <pro.dignitron@gmail.com>
2024-05-09 11:08:54 +0200

    initial commit

--- app.py
+++ app.py
@@ -46,7 +46,7 @@ app.config['SECRET_KEY'] = 'f9ec9f35fbf2a9d8b95f9bffd18ba9a1'
 # Online
 app.config["SQLALCHEMY_DATABASE_URI"] = "mysql+mysqldb://Tmaz:Tmazst41@Tmaz.mysql.pythonanywhere-services.com:3306/Tmaz$users_db"
 
-# if os.environ.get('ENV') == 'LOCAL':
+# if os.environ.get('ENV') == 'LOAL':
 #     app.config["SQLALCHEMY_DATABASE_URI"] = "mysql+mysqlconnector://root:tmazst41@localhost/tht_database"
 # else:
 #     app.config["SQLALCHEMY_DATABASE_URI"] = "mysql+mysqldb://Tmaz:Tmazst41@Tmaz.mysql.pythonanywhere-services.com:3306/Tmaz$users_db"
