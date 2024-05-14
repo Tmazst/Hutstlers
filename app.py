bf723a324e327ceb2d453838c1f699a771c075ab
Thabo <pro.dignitron@gmail.com>
2024-05-14 16:19:07 +0200

    initial commit

--- app.py
+++ app.py
@@ -1305,7 +1305,7 @@ def job_adverts_filtered():
 
 
         if not value == 'today' or not value == 'yesterday' or not value == 'this_week' or not value == 'this_month':
-            flash(f"Check Get Id: {value} ", "error")
+            flash(f"Check Get Id: {value} {type(value)}", "error")
             job_ads = Jobs_Ads.query.filter(Jobs_Ads.category.like(f"{value}%")).all()
         elif value == 'today' or value == 'yesterday' or value == 'this_week' or value == 'this_month':
             flash(f"Check Get Id: {value} ", "success")
